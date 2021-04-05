a3a96819d140d2cd71b73249e8a866e203335ade
ggwhsd <ggw_hsd@hotmail.com>
2021-04-05 22:59:36 +0800

    update

---
+++ src/baseSyntax/gateway.java
@@ -0,0 +1,53 @@
+package baseSyntax;
+
+public interface gateway {
+
+	public void Connect();
+	public void Close();
+	public void Login();
+	public void Logout();
+	public void ReqOrderInsert();
+	public void ReqOrderCancel();
+}
+
+class CTPGateway implements gateway
+{
+
+	@Override
+	public void Connect() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Login() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void Logout() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ReqOrderInsert() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void ReqOrderCancel() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
+
