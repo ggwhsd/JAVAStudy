b9fa8871bd1f0b002ae58011ffa9b86dfa1171d0
ggwhsd <ggw_hsd@hotmail.com>
2021-06-15 23:38:09 +0800

    update

--- OtherProjects/SpringBootRoute/src/main/java/com/studyspringcloud/SpringBootRouteApplication.java
+++ OtherProjects/SpringBootRoute/src/main/java/com/studyspringcloud/SpringBootRouteApplication.java
@@ -6,9 +6,26 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
+import com.studyspringcloud.filter.ElapsedFilter;
+
 @SpringBootApplication
 public class SpringBootRouteApplication {
 
+	//代码方式配置网关路由以及使用的自定义过滤器。
+	@Bean
+	public RouteLocator customerRouteLocator(RouteLocatorBuilder builder)
+	{
+		return builder.routes()
+				.route(r->r.path("/h2/**")
+						.filters(f->f.stripPrefix(1)
+								.filter(new ElapsedFilter())
+								.addResponseHeader("X-Response-Default-Foo", "Default-Bar")
+								.setPath("/hellos/hello1")
+								)
+						.uri("http://localhost:9001")
+						)
+				.build();
+	}
 	public static void main(String[] args) {
 		
 		SpringApplication.run(SpringBootRouteApplication.class, args);
