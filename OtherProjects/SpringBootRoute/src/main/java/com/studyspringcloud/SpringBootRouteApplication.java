0ac83e21c3253ea32a89f7b62def9767eb9a0a63
ggwhsd <ggw_hsd@hotmail.com>
2021-06-16 21:59:02 +0800

    update

--- OtherProjects/SpringBootRoute/src/main/java/com/studyspringcloud/SpringBootRouteApplication.java
+++ OtherProjects/SpringBootRoute/src/main/java/com/studyspringcloud/SpringBootRouteApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import com.studyspringcloud.filter.ElapsedFilter;
 
+
+@EnableFeignClients
 @SpringBootApplication
 public class SpringBootRouteApplication {
 
