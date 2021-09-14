83f14228fb53d8e7b16f4c7ec26bd90b4836bf4e
ggwhsd <ggw_hsd@hotmail.com>
2021-09-14 23:42:05 +0800

    update

--- OtherProjects/FileServerApp/src/main/java/com/example/demo/entity/OrderSpeedDateSum.java
+++ OtherProjects/FileServerApp/src/main/java/com/example/demo/entity/OrderSpeedDateSum.java
@@ -1,6 +1,24 @@
 package com.example.demo.entity;
 
 public class OrderSpeedDateSum {
+	public OrderSpeedDateSum(String actionDay, int sumOrder) {
+		super();
+		ActionDay = actionDay;
+		SumOrder = sumOrder;
+	}
+	public String getActionDay() {
+		return ActionDay;
+	}
+	public void setActionDay(String actionDay) {
+		ActionDay = actionDay;
+	}
+	public int getSumOrder() {
+		return SumOrder;
+	}
+	public void setSumOrder(int sumOrder) {
+		SumOrder = sumOrder;
+	}
+	private String ActionDay;
 private int SumOrder;
-private 
+
 }
